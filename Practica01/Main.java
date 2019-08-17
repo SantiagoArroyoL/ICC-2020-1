613ed1fa47acc5c98911f5d52ddd88f181f8b8e7
Santiago Arroyo <santiagoarroyo@ciencias.unam.mx>
2019-08-17 11:32:55 -0500

    Practicas 1 y 2 de ICC

---
+++ Practica01/Main.java
@@ -0,0 +1,15 @@
+/*Programador: Arroyo Lozano Santiago
+Fecha: 16/Agosto/2019
+Practica01
+*/
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println(
+						  "openjdk \"11.0.4\" 2019-07-16"
+									 + "\nOpenJDK Runtime Environment 18.9 (build 11.0.4+11)"
+									 + "\nOpenJDK 64-Bit Server VM 18.9 (build 11.0.4+11, mixed mode, sharing)"
+									 + "\n"
+									 + "\nLinux localhost.localdomain 5.2.6-200.fc30.x86_64 #1 SMP Mon Aug 5 13:20:47 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
+	}
+}
---
+++ Practica02/Sintaxis.java
@@ -0,0 +1,31 @@
+/*Programador: Arroyo Lozano Santiago
+Fecha: 16/Agosto/2019
+Practica02*/
+
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+	public static void main(String[] args) {
+		int a, b = 20, c;
+		a = 3; c = -7;
+
+		int prop = 0;
+		int ochoBits = 128;
+		int dieciseisBits = -32769;
+
+		double discriminante = b * b - 4 * a * c;
+		double x1 = (-b + sqrt(discriminante)) / (2 * a);
+		double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+		char ch1, ch2;
+		ch1 = 'g'; ch2 = 'w';
+		int d = 1 / (a - 2);
+
+		// Declaraciones
+
+		int var_1 = 2147483647; // Nota en soluciones.txt
+		char quote = '\'';
+	}
+
+}
