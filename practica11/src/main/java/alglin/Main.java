d087ecf04dc469fb9bb55488add29851e88f7e21
Santiago Arroyo <santiagoarroyo@ciencias.unam.mx>
2019-10-17 22:10:48 -0500

    Actualizacion practica 10 y 11

---
+++ practica11/src/main/java/alglin/Main.java
@@ -0,0 +1,8 @@
+public  class Main{
+	public static void main(String[] args) {
+		Matriz prueba = new Matriz(10,10);
+		Matriz prueba1 = new Matriz(10,10);
+		System.out.println(prueba);
+		System.out.println(prueba.sumar(prueba1));
+	}
+}
