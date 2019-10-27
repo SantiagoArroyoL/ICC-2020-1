9731ccfcd031bf39752752608e0600fa6cfe4227
Santiago Arroyo <santiagoarroyo@ciencias.unam.mx>
2019-10-27 16:49:01 -0600

    Practica13 Completa

---
+++ practica13/src/main/java/ajedrez/piezas/Dama.java
@@ -0,0 +1,133 @@
+package ajedrez.piezas;
+import ajedrez.Tablero;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * Clase Dama.
+ *
+ * @author Arroyo Lozano Santiago
+ * @version 24/10/2019 A
+ */
+public class Dama extends Pieza{
+
+	public Dama(Color color, Posicion posicion) {
+		super(color,posicion);
+	}
+
+	Tablero tab = Tablero.obtenerInstancia();
+
+	@Override
+	public List<Posicion> obtenerJugadasLegales() {
+
+		int n = 1;
+		int m = 1;
+		LinkedList<Posicion> jugadas = new LinkedList<>();
+		int fila = obtenerPosicion().obtenerFila();
+		int columna = obtenerPosicion().obtenerColumna();
+
+		//For de las columnas
+		for (int i = columna; i <= 7; i++) {
+			Pieza p1 = tab.obtenerPieza(fila,i);
+			for (int j = columna; j >= 0; j--) {
+				Pieza p2 = tab.obtenerPieza(fila,j);
+				if (p1 == null && columna + n <= 7) {
+				// if (columna + n <= 7) {
+					jugadas.add(new Posicion(fila, columna + n));
+					n++;
+				} if (p2 == null && columna - m >= 0) {
+				} if (columna - m >= 0) {
+						jugadas.add(new Posicion(fila, columna - m));
+						m++;
+				}  else if (p2.obtenerColor() != obtenerColor()) {
+					jugadas.add(new Posicion(fila, columna - (m - 1)));
+					break;
+				}
+			}
+			if (p1.obtenerColor() != obtenerColor()) {
+				jugadas.add(new Posicion(fila, columna + (n +1)));
+				break;
+			}
+		}
+
+		n = 1;
+		m = 1;
+		//For de las filas
+		for (int i = fila; i <= 7; i++) {
+			Pieza p1 = tab.obtenerPieza(i,i);
+			for (int j = fila; j >= 0; j--) {
+				Pieza p2 = tab.obtenerPieza(j,j);
+				if (p1 == null && fila + n <= 7) {
+				if (fila + n <= 7) {
+					jugadas.add(new Posicion(fila + n, columna));
+					n++;
+				}  if (p2 == null && fila - m >= 0) {
+				// }  if (fila - m >= 0) {
+						jugadas.add(new Posicion(fila - m , columna));
+						m++;
+				} else if (p2.obtenerColor() != obtenerColor()) {
+					jugadas.add(new Posicion(fila - (m - 1), columna));
+					break;
+				}
+			}
+			if (p1.obtenerColor() != obtenerColor()) {
+				jugadas.add(new Posicion(fila + (n +1), columna ));
+				break;
+			}
+		}
+
+		n = 1;
+		m = 1;
+		//For de las diagonales 1
+		for (int i = fila; i <= 7; i++) {
+			Pieza p1 = tab.obtenerPieza(i,i);
+			for (int j = fila; j >= 0; j--) {
+				Pieza p2 = tab.obtenerPieza(j,j);
+				if ((p1 == null && fila + n <= 7 ) && columna - n >= 0) {
+				// if (fila + n <= 7 && columna - n >= 0) {
+					jugadas.add(new Posicion(fila + n, columna - n));
+					n++;
+				} if ((p2 == null && columna + m <= 7) && fila - m >= 0) {
+				// } if (columna + m <= 7 && fila - m >= 0) {
+						jugadas.add(new Posicion(fila -m , columna + m));
+						m++;
+				}  else if (p2.obtenerColor() != obtenerColor()) {
+					jugadas.add(new Posicion(fila - (m - 1), columna + (m - 1)));
+					break;
+				}
+			}
+			if (p1.obtenerColor() != obtenerColor()) {
+				jugadas.add(new Posicion(fila + (n + 1), columna - (n + 1) ));
+				break;
+			}
+		}
+
+		n = 1;
+		m = 1;
+		//For de las diagonales 2
+		for (int i = fila; i <= 7; i++) {
+			Pieza p1 = tab.obtenerPieza(i,columna);
+			for (int j = fila; j >= 0; j--) {
+				Pieza p2 = tab.obtenerPieza(j,columna);
+				if ((p1 == null && columna + n <= 7) && fila + n <= 7) {
+				// if (columna + n <= 7 && fila + n <= 7) {
+					jugadas.add(new Posicion(fila + n, columna + n));
+					n++;
+				} if ((p2 == null && columna - m >= 0) && fila -n >= 0) {
+				// } if (columna - m >= 0 && fila -n >= 0) {
+						jugadas.add(new Posicion(fila - m , columna - m));
+						m++;
+				} else if (p2.obtenerColor() != obtenerColor()) {
+					jugadas.add(new Posicion(fila - (m - 1), columna - (m - 1) ));
+					break;
+				}
+			}
+			if (p1.obtenerColor() != obtenerColor()) {
+				jugadas.add(new Posicion(fila + (n + 1), columna + (n + 1) ));
+				break;
+			}
+		}
+		return jugadas;
+		}
+	}
+}
---
+++ practica13/src/main/java/ajedrez/piezas/Posicion.java
@@ -0,0 +1,58 @@
+package ajedrez.piezas;
+
+/**
+ * Clase que representa la posición de cada pieza.
+ *
+ * @author Arroyo Lozano Santiago
+ * @version 22/10/2019 A
+ */
+public class Posicion {
+	private int fila;
+	private int columna;
+
+	/**
+	 * Crea una matriz de mxn donde todas las entradas son cero.
+	 *
+	 * @param fila donde ese encuentra la pieza
+	 * @param columna donde se encuentra l apieza
+	 */
+	public Posicion(int fila, int columna) {
+		this.fila = fila;
+		this.columna = columna;
+	}//Cierre del método
+
+	/**
+	 * método para obtener la fila de la Pieza
+	 * @return fila donde se encuentra la Pieza
+	 */
+	public int obtenerFila() {
+		return fila;
+	}//Cierre del método
+
+	/**
+	 * método para obtener la columna de la Pieza
+	 * @return columna donde se encuentra la Pieza
+	 */
+	public int obtenerColumna() {
+		return columna;
+	}//Cierre del método
+
+	/**
+	 * método para obtener la columna de la Pieza
+	 * @param obj El objeto a comparar
+	 * @return Verdadero si son iguales, falso si no lo son
+	 */
+	@Override
+	public boolean equals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+
+		if (obj == null || getClass() != obj.getClass()) {
+         return false;
+      }
+		Posicion otra = (Posicion) obj;
+		return fila == otra.fila && columna == otra.columna;
+	}
+
+}//Cierre de la clase
